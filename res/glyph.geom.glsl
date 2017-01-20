9fe60596d3babb4da951c77ffd426535d11b711e
Jakob Bornecrantz <wallbraker@gmail.com>
2017-01-20 04:33:19 +0100

    ui: Correctly define gl_Position

--- res/glyph.geom.glsl
+++ res/glyph.geom.glsl
@@ -9,7 +9,10 @@ layout (location = 1) in vec3[] inGlyph;
 layout (triangle_strip, max_vertices = 4) out;
 layout (location = 0) out vec3 outGlyph;
 layout (location = 1) out vec2 outUV;
-out vec4 gl_Position;
+out gl_PerVertex
+{
+	vec4 gl_Position;
+};
 
 #define minX inPosition[0].x
 #define minY inPosition[0].y
