025181c271a406b9756149d503477e86885c191e
Jakob Bornecrantz <wallbraker@gmail.com>
2017-01-20 12:12:19 +0100

    ui: Remove GL_ARB_shading_language_420pack

--- res/glyph.frag.glsl
+++ res/glyph.frag.glsl
@@ -1,13 +1,12 @@
 #version 330 core
 #extension GL_ARB_explicit_attrib_location : require
 #extension GL_ARB_separate_shader_objects : require
-#extension GL_ARB_shading_language_420pack : require
 
 layout (location = 0) in vec3 inGlyph;
 layout (location = 1) in vec2 inUV;
 layout (location = 0) out vec4 outColor;
 
-layout (binding = 0) uniform sampler2DArray tex;
+uniform sampler2DArray tex;
 
 #define glyphIndex inGlyph.x
 #define fgColor inGlyph.y
--- res/glyph.vert.glsl
+++ res/glyph.vert.glsl
@@ -1,13 +1,12 @@
 #version 330 core
 #extension GL_ARB_explicit_attrib_location : require
 #extension GL_ARB_separate_shader_objects : require
-#extension GL_ARB_shading_language_420pack : require
 
 layout (location = 0) in vec4 inData;
 layout (location = 0) out vec4 outPosition;
 layout (location = 1) out vec3 outGlyph;
 
-layout (binding = 0, std140) uniform Uniforms {
+layout (std140) uniform Uniforms {
 	vec4 info;
 };
 
